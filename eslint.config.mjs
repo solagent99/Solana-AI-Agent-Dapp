5ce855cda2d2cba0a8963f6836e9dfef3e87355f
solagent99 <davidanderson9469@gmail.com>
2025-01-05 08:34:20 +0000

    chore: add ESLint configuration for easier Linux deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,27 @@
-import eslintGlobalConfig from "../../eslint.config.mjs";
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
-export default [...eslintGlobalConfig];
+export default {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-empty-function': 'warn',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  ignorePatterns: ['dist/**/*', 'node_modules/**/*'],
+};
